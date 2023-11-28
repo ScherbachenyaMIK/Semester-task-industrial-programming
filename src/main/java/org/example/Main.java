3abb60474a88bbb756327f118c56bedd41844a72
Mikhail Scherbachenya <mikhail221724@gmail.com>
2023-11-28 11:24:06 +0300

    Encoder and Decoder have been added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,32 @@
 package org.example;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidParameterSpecException;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        DearchiverZip dz = new DearchiverZip("Tasks1-2.zip");
+    public static void main(String[] args) throws IOException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException, InvalidParameterSpecException {
+        Decoder de = new Decoder("EncTasks1-2.zip", "DecTasks1-2.zip");
+        de.decryptFile("ssdkF$HUy2A#D%kd");
+        de.closeDecoder();
+        DearchiverZip dz = new DearchiverZip("DecTasks1-2.zip");
         ArrayList<String> LA = dz.Dearchive();
         for(String i : LA)
         {
             System.out.println(i);
         }
-        ArchiverZip az = new ArchiverZip(LA.get(0).substring(0, LA.get(0).indexOf('/')) + "Tasks1-2_result.zip");
+        ArchiverZip az = new ArchiverZip("Tasks1-2_result.zip");
         az.Archive(LA, "./");
         dz.CloseDearchiverZip();
         az.CloseArchiverZip();
+        Encoder en = new Encoder("Tasks1-2_result.zip", "EncTasks1-2_result.zip");
+        en.encryptFile("ssdkF$HUy2A#D%kd");
+        en.CloseEncoder();
     }
 }
